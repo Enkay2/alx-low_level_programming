5a30a3688afd6cc0501da7b46f6602d31674759e
Enkay2 <sylviankay@gmail.com>
2022-09-11 14:58:52 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints a string
+ * Description - Prints a string using puts not printf
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
