f40f2f6e4f56befd2f130d2e73ec9325b604c9df
Enkay2 <sylviankay@gmail.com>
2022-09-12 07:16:21 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,35 +6,13 @@
  */
 int main(void)
 {
-	int a;
-	int b;
-	int c;
+	int i;
 
-	for (a = 0; a <= 9; a++)
-
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (b = 0; b <= 9; b++)
-
-		{
-			for (c = 0; c <= 9; c++)
-
-				if (c > b && b > a)
-
-				{
-					putchar(a + '0');
-					putchar(b + '0');
-					putchar(c + '0');
-					if (a != 7 || b != 8 || c != 9)
-
-					{
-						putchar(',');
-						putchar(' ');
-					}
-
-				}
-
-		}
-
+	putchar(i);
+	putchar(',');
+	putchar(' ');
 	}
 	putchar('\n');
 	return (0);
