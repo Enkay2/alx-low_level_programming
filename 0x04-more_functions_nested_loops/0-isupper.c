d2ec629906563af34a63464afe470a4ca5c684f4
Enkay2 <sylviankay@gmail.com>
2022-09-15 05:38:14 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase character
+ * @c: letter is being tested
+ * Description: _isupper - prints _putchar message
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 65 && c <= 90)
+
+	{
+		return (1);
+	}
+
+		return (0);
+}
+
